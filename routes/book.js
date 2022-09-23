4212f3e50f670d83262786413b5846481b74b9ce
markand <markandyuvasoft408@gmail.com>
2022-09-23 22:30:27 +0530

    daads

--- routes/book.js
+++ routes/book.js
@@ -3,50 +3,6 @@ import Book from "../models/book.js";
 const router=express.Router()
 
 
-// POST: to create the book
-// router.post('/book',(req,res)=>{
-    
-//     const book=new Book({
-//         name:req.body.bookname,
-//         author:{
-//             name:req.body.bookname,
-//             age:req.body.authorage
-//         },
-//         genre:req.body.genre
-
-//     });
-//     book.save().then(book=>{
-//         res.send(book);
-//     }).catch(error=>{
-//         res.status(500).send("book was not stored in db");
-//     })
-// })
-
-
-router.get("/get",(req,res)=>{
-    res.send("home")
-})
-
-
-// router.post('/post', async (req, res) => {
-//     const data = new Book({
-//         name:req.body.bookname,
-//         author:{
-//             name:req.body.authorname,
-//             age:req.body.authorage
-//         },
-//         genre:req.body.genre
-//     })
-
-//     try {
-//         const dataToSave = await data.save();
-//         res.status(200).json(dataToSave)
-//     }
-//     catch (error) {
-//         res.status(400).json({message: error.message})
-//     }
-// })
-
 router.post("/post",(req,res,next)=>{
 
     const user = new Book(req.body)
@@ -62,5 +18,50 @@ router.post("/post",(req,res,next)=>{
     }) 
   })
 
+
+  router.get("/get",async(req,res)=>{
+
+    try{
+
+    const get= await Book.find()
+
+    res.status(201).send(get)
+    }
+    catch(err)
+    {
+    res.status(400).send(err)
+    }
+})
+
+router.put("/update/:id", async (req,res)=>{
+    try{
+    
+    const _id= req.params.id;
+    
+    const update= await Book.findByIdAndUpdate(_id, req.body)
+    
+    res.send(update)
+    
+    }catch(err){
+    
+    res.status(400).send(err)
+    }
+    })
+
+
+    router.delete("/delete/:id",async(req,res)=>{
+
+        try{
+            const _id= req.params.id
+    
+            const del= await Book.findByIdAndDelete(_id)
+    
+            res.status(201).send(del)
+        }
+        catch(err)
+        {
+            res.status(500).send(err)
+        }
+    })
 export default router
 
