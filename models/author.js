bd0fad8b8b871d8fe7e9c16fd6b31ca0ed859229
markand <markandyuvasoft408@gmail.com>
2022-09-23 21:12:57 +0530

    a

--- models/author.js
+++ models/author.js
@@ -5,18 +5,14 @@ import mongoose from "mongoose";
 const authorSchema=new mongoose.Schema({
     name:{
         type:String,
-        required:true,
-        minlength:3,
-        maxlength:40
     },
     age:{
         
         type:Number,
-        minlength:10,
-        maxlength:100
     }
 
 })
 
 const Author=mongoose.model('Author',authorSchema)
+
 export default Author
